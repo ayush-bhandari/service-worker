2fff163838ef21e694b4ee16ec7eda511a453911
ayushbhandari <ayush@kozzaja.com>
2017-02-20 00:48:38 +0545

    adding files to cache in install event

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,14 @@
+var cacheName = 'cacheName';
+var cacheFiles = ['index.html','app.js'];
+
 this.addEventListener('install', function(event){
 	console.log("Service Worker install event");
+	event.waitUntil(
+		caches.open(cacheName).then(function(cache){
+			console.log("Adding files to cache memory");
+			return cache.addAll(cacheFiles);
+		})
+	)
 })
 this.addEventListener('activate', function(event){
 	console.log("Service Worker activate event");
