edf85b86c6deeb6658d5c702cf890d7232831aa4
ayushbhandari <ayush@kozzaja.com>
2017-02-19 22:45:34 +0545

    xmlhttp request from app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+function getImage(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	    	document.getElementsByClassName('image')[0].src = "https://placeholdit.imgix.net/~text?txtsize=56&bg=92c952&txt=300%C3%97300&w=300&h=300";
+	    }
+	};
+	xhttp.open("GET", "https://placeholdit.imgix.net/~text?txtsize=56&bg=92c952&txt=300%C3%97300&w=300&h=300", true);
+	xhttp.send();
+}
+getImage();
